91f5078f50c5f32b37da588ddaa6c5bfb40d7d03
KainAzeroth <bsmalanga@gmail.com>
2019-11-15 23:18:01 -0800

    Attempting to limit the file produced in a pretty janky manner.

--- pitemp0.1.py
+++ pitemp0.1.py
@@ -10,9 +10,17 @@ import datetime
 # This example shows how to get the temperature from a MCP9808 board
 i2c_bus = busio.I2C(board.SCL, board.SDA)
 mcp = adafruit_mcp9808.MCP9808(i2c_bus)
+maxPoints = 1000
+count = 0
 
 while True:
-    tempFile = open("temp.txt", "a")
+    if count < maxPoints:
+        tempFile = open("temp.txt", "a")
+        count = 0
+    if count > maxPoints:
+        tempFile = open("temp.txt", "w")
+        count = count + 1
+    
     tempC = mcp.temperature
     tempF = tempC * 9 / 5 + 32
     currentDT = time.strftime('%X %x %Z')
@@ -21,4 +29,3 @@ while True:
     print(reportedTemp, file=tempFile)
     time.sleep(2)
     tempFile.close()
-
