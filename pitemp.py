26307c50c3f2bee17e6711ca714b499987d412de
KainAzeroth <bsmalanga@gmail.com>
2019-11-19 01:00:31 -0800

    created variable sleepTimer as a type of polling rate for the sensor.

--- pitemp.py
+++ pitemp.py
@@ -12,9 +12,13 @@ i2c_bus = busio.I2C(board.SCL, board.SDA)
 mcp = adafruit_mcp9808.MCP9808(i2c_bus)
 
 maxPoints = 5000 #Max data points to keep in the file. Really badly done. Restarts the file at this number instead of removing a line and adding a new one
+sleepTimer = 2 #This is the number of seconds between checking the sensor. Kinda like a polling rate in seconds. Values between 1 and 3600 seconds
 count = 0
 
 while True:
+    #checking to make sure the sleepTimer isn't set to something stupid
+    if
+
     tempC = mcp.temperature
     tempF = tempC * 9 / 5 + 32
     currentDT = time.strftime('%X %x %Z')
@@ -37,12 +41,12 @@ while True:
         tempFile = open("temp.txt", "a")
         print(reportedTemp, file=tempFile)
         count = count + 1
-        time.sleep(2)
+        time.sleep(sleepTimer)
         tempFile.close()
 
     if count >= maxPoints:
         tempFile = open("temp.txt", "w")
         print(reportedTemp, file=tempFile)
         count = 0
-        time.sleep(2)
+        time.sleep(sleepTimer)
         tempFile.close()
