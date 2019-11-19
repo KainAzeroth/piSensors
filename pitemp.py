449c539c3aff46cab7a5eb908d36fafcc514a5df
KainAzeroth <bsmalanga@gmail.com>
2019-11-18 22:43:09 -0800

    Added detecting basic ranges of temperatures and added some commenting and fixed and error.

--- pitemp.py
+++ pitemp.py
@@ -10,7 +10,8 @@ import datetime
 # This example shows how to get the temperature from a MCP9808 board
 i2c_bus = busio.I2C(board.SCL, board.SDA)
 mcp = adafruit_mcp9808.MCP9808(i2c_bus)
-maxPoints = 5000
+
+maxPoints = 5000 #Max data points to keep in the file. Really badly done. Restarts the file at this number instead of removing a line and adding a new one
 count = 0
 
 while True:
@@ -18,6 +19,18 @@ while True:
     tempF = tempC * 9 / 5 + 32
     currentDT = time.strftime('%X %x %Z')
     reportedTemp = f"{currentDT}\tTemp: {tempC} C | {tempF} F | {count} Count"
+
+    if tempF < 50:
+        reportedTemp = reportedTemp + " COLD WARNING! HOT WARNING!"
+    elif tempf < 60:
+        reportedTemp = reportedTemp + " Cool"
+    elif tempf < 70:
+        reportedTemp = reportedTemp + " Moderate"
+    elif tempf < 80:
+        reportedTemp = reportedTemp + " Warm"
+    elif tempf < 90:
+        reportedTemp = reportedTemp + " HOT WARNING! HOT WARNING!"
+
     print(reportedTemp)
 
     if count < maxPoints:
