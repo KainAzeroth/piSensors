fe7eb84188dcc34138bb187dad1aa663248437fc
KainAzeroth <bsmalanga@gmail.com>
2021-12-19 00:07:41 -0600

    Testing

--- pisensors.py
+++ pisensors.py
@@ -7,6 +7,9 @@ import busio
 import adafruit_mcp9808
 import adafruit_sht31d
 
+#plotext Imports
+import plotext as plt
+
 # Adding color with colorama
 import colorama
 from colorama import Fore, Style
@@ -18,7 +21,7 @@ sht = adafruit_sht31d.SHT31D(i2c_bus, 0x44)
 
 # Settings
 maxPoints = 5000 #Max data points to keep in the file. Really badly done. Restarts the file at this number instead of removing a line and adding a new one
-sleepTimer = 1 #This is the number of seconds between checking the sensor. Kinda like a polling rate in seconds. Values between 1 and 3600 seconds
+sleepTimer = 5 #This is the number of seconds between checking the sensor. Kinda like a polling rate in seconds. Values between 1 and 3600 seconds
 lowTemp = 75 #Warning when temp is below this
 highTemp = 80 #Warning when temp is above this
 
@@ -30,7 +33,7 @@ while True:
     #checking to make sure the sleepTimer isn't set to something stupid
     if sleepTimer < 1: #if less than 1 set to 1. Minimum is 1 second.
         sleepTimer = 1
-    elif sleepTimer > 3600: #if more than 3600 set to 3600. This is 1 hour in seconds. More than that seems silly.         
+    elif sleepTimer > 3600: #if more than 3600 set to 3600. This is 1 hour in seconds. More than that seems silly.
         sleepTimer = 3600
 
     currentDT = time.strftime('%X %x %Z')
@@ -46,31 +49,31 @@ while True:
     shtTempFR = round(shtTempF,3)
     shtHumidity = format(round(sht.relative_humidity,3), '.3f')
 
-    reportedTemp = f"{currentDT} | {mcpTempCR} C | {mcpTempFR} F | {shtHumidity}%"
+#    reportedTemp = f"{currentDT} | {mcpTempCR} C | {mcpTempFR} F | {shtHumidity}%"
 
 #    OLD FORMAT
 #    reportedTemp = f"{currentDT} | {mcpTempCR} mC | {mcpTempFR} mF | {shtTempCR} sC | {shtTempFR} sF | {shtHumidity} % RH"
 
-    if mcpTempF > highTemp:
-        print(Fore.RED + reportedTemp + Style.RESET_ALL)
-    elif mcpTempF < lowTemp:
-        print(Fore.CYAN + reportedTemp +Style.RESET_ALL)
-    else:
-        print(reportedTemp)
+#    if mcpTempF > highTemp:
+#        print(Fore.RED + reportedTemp + Style.RESET_ALL)
+#    elif mcpTempF < lowTemp:
+#        print(Fore.CYAN + reportedTemp +Style.RESET_ALL)
+#   else:
+#        print(reportedTemp)
 
-    if countA < maxPoints:
-        tempFile = open("temp.txt", "a")
-        print(reportedTemp, file=tempFile)
-        countA = countA + 1
-        time.sleep(sleepTimer)
-        tempFile.close()
+#    if countA < maxPoints:
+#        tempFile = open("temp.txt", "a")
+#        print(reportedTemp, file=tempFile)
+#        countA = countA + 1
+#        time.sleep(sleepTimer)
+#        tempFile.close()
 
-    if countA >= maxPoints:
-        tempFile = open("temp.txt", "w")
-        print(reportedTemp, file=tempFile)
-        countA = 0
-        time.sleep(sleepTimer)
-        tempFile.close()
+#    if countA >= maxPoints:
+#        tempFile = open("temp.txt", "w")
+#        print(reportedTemp, file=tempFile)
+#        countA = 0
+#        time.sleep(sleepTimer)
+#        tempFile.close()
 
     loopcount += 1
 
@@ -83,3 +86,22 @@ while True:
         time.sleep(1)
         sht.heater = False
 #        print("SHT31-D Heater status =", sht.heater)
+
+# Plotext plotting
+
+
+    plt.title("Temperature")
+    plt.clc()
+    frames = 50
+    x = range(1, 200+1)
+
+    for i in range(frames):
+        y = mcp.temperature * 1.8 + 32
+
+        plt.cld()
+        plt.clt()
+        plt.plot(x, y, marker = "dot")
+
+#        plt.sleep(0.001)
+        plt.show()
+
